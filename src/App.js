a44a798b2de9d34a7eb3e9d03d61950a85608d7e
rysxa <indrysfa@gmail.com>
2024-03-14 13:55:31 +0700

    install tailwind & router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './layout/Header';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
--- src/layout/Routers.jsx
+++ src/layout/Routers.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import LooppingList from './training/LoopingList';
-import Props from './training/Props';
 
 const Routers = () => {
   return (
